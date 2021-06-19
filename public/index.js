db5c32cc2762f74d50c0253018da3b453f948963
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 13:52:15 -0700

    refactored index.js

--- public/index.js
+++ public/index.js
@@ -2,20 +2,28 @@ const container = document.querySelector(".screenshot");
 const button = document.querySelector("#button");
 
 async function onButtonClick() {
-  const screen = await fetch("/.netlify/functions/flash-background").then(
-    async (response) => await response.json()
+  // const screen = await fetch("/.netlify/functions/flash").then((response) => response.json());
+  const screenGrab = await fetch("/.netlify/functions/flash").then(
+    (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        const resError = response.text();
+        return Promise.reject(new Error(resError));
+      }
+    }
   );
 
   const img = document.createElement("img");
-  const loadText = document.createElement("p");
+  const loadingMessage = document.createElement("span");
 
-  if (!screen) {
-    loadText.innerText("Loading...");
-    container.appendChild(loadText);
-  }
-  img.src = screen;
-  img.alt = "solidiceman on twitch";
-  container.appendChild(img);
+  img.src = screenGrab;
+  img.alt = "thesolidiceman twitch channel";
+  loadingMessage.innerText = "Loading...";
+
+  screenGrab
+    ? container.appendChild(img)
+    : container.appendChild(loadingMessage);
 }
 
 button.addEventListener("click", onButtonClick);
