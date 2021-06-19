7a0e2da20f2c0b544095bf647485873f9b3e63d8
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 14:53:20 -0700

    update

--- functions/flash.js
+++ functions/flash.js
@@ -9,30 +9,13 @@ exports.handler = async () => {
   api.searchParams.set("access_key", process.env.ACCESS_KEY);
   api.searchParams.set("url", urlToView);
 
-  // const data = await fetch(api);
-  // if (!response.ok) {
-  //   throw new Error(
-  //     "There was an error, please refresh your browser and try again"
-  //   );
-  // }
-
-  const getData = async (apiUrl) => {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(
-        "There was an error, please refresh your browser and try again"
-      );
-    }
-    return response
-      .buffer()
-      .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
-  };
-
-  const data = getData(api);
-
-  // const data = await fetch(api)
-  //   .then((response) => response.buffer())
-  //   .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
+  const data = await fetch(api)
+    .then((response) => response.buffer())
+    .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"))
+    .catch((error) => {
+      console.error("error:", error);
+      return "error";
+    });
 
   return {
     statusCode: 200,
