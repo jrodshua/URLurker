6158d9301993ad5e471d721c5f1f68cfda179453
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 12:19:33 -0700

    refactored index.js, shortned img url

--- public/index.js
+++ public/index.js
@@ -2,9 +2,12 @@ const container = document.querySelector(".screenshot");
 const button = document.querySelector("#button");
 
 async function onButtonClick() {
-  const screen = await fetch("/.netlify/functions/flash").then((response) =>
-    response.json()
-  );
+  const screen = await fetch("/.netlify/functions/flash")
+    .then((response) => response.blob())
+    .then((blob) => {
+      const screenshot = URL.createObjectURL(blob);
+      return screenshot;
+    });
 
   const img = document.createElement("img");
   img.src = screen;
