ed018477158f52dd379e46dcb18b381a4fe8b466
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 12:06:54 -0700

    refactored netlify flash function

--- functions/flash.js
+++ functions/flash.js
@@ -1,20 +1,38 @@
 require("dotenv").config();
 const { URL } = require("url");
+// const { writeFile } = require('fs');
 const fetch = require("node-fetch");
 
 exports.handler = async () => {
   const api = new URL("https://api.apiflash.com/v1/urltoimage");
-
   const urlToView = new URL("http://twitch.com/");
 
   api.searchParams.set("access_key", process.env.ACCESS_KEY);
-  api.searchParams.set("response_type", "json");
   api.searchParams.set("url", urlToView);
 
-  const getFlash = await fetch(api).then((response) => response.json());
+  const data = await fetch(api)
+    .then((response) => response.buffer())
+    .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
 
   return {
     statusCode: 200,
-    body: JSON.stringify(getFlash),
+    body: JSON.stringify(data),
   };
 };
+
+// exports.handler = async () => {
+//   const api = new URL("https://api.apiflash.com/v1/urltoimage");
+
+//   const urlToView = new URL("http://twitch.com/");
+
+//   api.searchParams.set("access_key", process.env.ACCESS_KEY);
+//   api.searchParams.set("response_type", "json");
+//   api.searchParams.set("url", urlToView);
+
+//   const getFlash = await fetch(api).then((response) => response.json());
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(getFlash),
+//   };
+// };
