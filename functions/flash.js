523fc5b1801b3f6778ec36a784feb03902678a18
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 14:18:53 -0700

    flash.js refactor

--- functions/flash.js
+++ functions/flash.js
@@ -5,34 +5,37 @@ const fetch = require("node-fetch");
 
 exports.handler = async () => {
   const api = new URL("https://api.apiflash.com/v1/urltoimage");
-  const urlToView = new URL("https://www.twitch.tv/thesolidiceman");
+  const urlToView = new URL("https://www.twitch.tv/jrodshua");
 
   api.searchParams.set("access_key", process.env.ACCESS_KEY);
   api.searchParams.set("url", urlToView);
 
-  const data = await fetch(api)
-    .then((response) => response.buffer())
-    .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
+  function getScreenShot(apiUrl, retry = 2) {
+    return fetch(apiUrl)
+      .then(async (response) => {
+        if (response.ok) {
+          return await response
+            .buffer()
+            .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
+        }
+
+        if (retry > 0) {
+          return getScreenShot(apiUrl, retry - 1);
+        } else {
+          throw new Error(response);
+        }
+      })
+      .catch(console.error);
+  }
+
+  const data = getScreenShot(api);
+
+  // const data = await fetch(api)
+  //   .then((response) => response.buffer())
+  //   .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
 
   return {
     statusCode: 200,
     body: JSON.stringify(data),
   };
 };
-
-// exports.handler = async () => {
-//   const api = new URL("https://api.apiflash.com/v1/urltoimage");
-
-//   const urlToView = new URL("http://twitch.com/");
-
-//   api.searchParams.set("access_key", process.env.ACCESS_KEY);
-//   api.searchParams.set("response_type", "json");
-//   api.searchParams.set("url", urlToView);
-
-//   const getFlash = await fetch(api).then((response) => response.json());
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(getFlash),
-//   };
-// };
