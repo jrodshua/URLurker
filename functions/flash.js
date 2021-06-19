9d63e36ed937dd7a90447ecca4d6a8d00b47908f
Josh Rodriguez <jrodshua@gmail.com>
2021-06-19 15:50:43 -0700

    added form to html, updated index to post, updated flash.js

--- functions/flash.js
+++ functions/flash.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const { URL } = require("url");
 const fetch = require("node-fetch");
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  const { url } = JSON.parse(event.body);
+
   const api = new URL("https://api.apiflash.com/v1/urltoimage");
-  const urlToView = new URL("https://www.twitch.tv/jrodshua");
+  const formUrl = new URL(url);
 
   api.searchParams.set("access_key", process.env.ACCESS_KEY);
-  api.searchParams.set("url", urlToView);
+  api.searchParams.set("url", formUrl);
 
   const data = await fetch(api)
     .then((response) => response.buffer())
@@ -18,3 +20,20 @@ exports.handler = async () => {
     body: JSON.stringify(data),
   };
 };
+
+// exports.handler = async () => {
+//   const api = new URL("https://api.apiflash.com/v1/urltoimage");
+//   const urlToView = new URL("https://www.twitch.tv/jrodshua");
+
+//   api.searchParams.set("access_key", process.env.ACCESS_KEY);
+//   api.searchParams.set("url", urlToView);
+
+//   const data = await fetch(api)
+//     .then((response) => response.buffer())
+//     .then((buf) => `data:image/jpeg;base64,` + buf.toString("base64"));
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(data),
+//   };
+// };
--- public/index.js
+++ public/index.js
@@ -1,26 +1,47 @@
 const container = document.querySelector(".screenshot");
-const button = document.querySelector("#button");
+// const button = document.querySelector("#button");
+const form = document.querySelector("#api-form");
 
-async function getScreenShot() {
-  const data = await fetch("/.netlify/functions/flash")
-    .then((response) => response.json())
-    .catch((error) => error);
+// async function getScreenShot() {
+//   const data = await fetch("/.netlify/functions/flash")
+//     .then((response) => response.json())
+//     .catch((error) => error);
 
-  return data;
-}
+//   return data;
+// }
 
-async function onButtonClick() {
-  const apiData = await getScreenShot();
+// async function onButtonClick() {
+//   const apiData = await getScreenShot();
 
-  if (!apiData) {
-    const errorText = document.createElement("span");
-    errorText.innerText = "there was an error, please try again";
-    container.appendChild(errorText);
-  }
+//   if (!apiData) {
+//     const errorText = document.createElement("span");
+//     errorText.innerText = "there was an error, please try again";
+//     container.appendChild(errorText);
+//   }
+//   const img = document.createElement("img");
+//   img.src = apiData;
+//   img.alt = "jrodshua twitch";
+//   container.appendChild(img);
+// }
+
+// button.addEventListener("click", onButtonClick);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const result = await fetch("/.netlify/functions/flash", {
+    method: "POST",
+    body: JSON.stringify({
+      url: data.get("form-url"),
+    }),
+  }).then((response) => response.json());
+
   const img = document.createElement("img");
-  img.src = apiData;
+  img.src = result;
   img.alt = "jrodshua twitch";
   container.appendChild(img);
 }
 
-button.addEventListener("click", onButtonClick);
+form.addEventListener("submit", handleSubmit);
